bb6f3435c0ad5cfdee9d05fbf7941c7726aacfb8
andreypolyak <andrey.polyak@gmail.com>
2016-12-10 00:01:35 +0300

    Width optimization

--- gitbook/gitbook-plugin-hide-published-with/plugin.js
+++ gitbook/gitbook-plugin-hide-published-with/plugin.js
@@ -1,7 +1,7 @@
 require(["gitbook"], function(gitbook) {
 	$('.gitbook-link').hide(); 
     gitbook.events.bind("page.change", function() {
-    	console.log("Hello custom plugin")
+    	//console.log("Hello custom plugin")
     	$('.gitbook-link').hide();           
     });
 });
