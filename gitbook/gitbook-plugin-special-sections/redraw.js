a3138daa4720f806f7f234deeb6b9c6f635d230b
andreypolyak <andrey.polyak@gmail.com>
2017-02-14 15:07:27 +0300

    Create booking

--- gitbook/gitbook-plugin-special-sections/redraw.js
+++ gitbook/gitbook-plugin-special-sections/redraw.js
@@ -1,11 +1,27 @@
-function redraw(item) {
-	var show = 0;
-	var type = $(item).data('type');
-	$('.sws-checkbox[data-type=' + type + ']').each(function(i, el) {
-		if ($(el).is(":checked")) {
-			show = show + $(el).data('id');
-		}
-	});
-	$('.sws-div[data-type=' + type + ']').hide();
-	$('[data-id=' + show + '][data-type=' + type + ']').show();
+function redraw(id) {
+  var show = 0;
+  $(`.sws-checkbox`).each(function(i, el) {
+    if ($(el).is(':checked')) {
+      show = show + $(el).data('id');
+    }
+  });
+  $(`.panel-image-group`).hide();
+  $(`.panel-image-group[data-id=${show}]`).show();
 }
+
+$('.panel').on('hide.bs.collapse', function (el) {
+  $('#toggleangle' + el.target.id.replace('collapse', '')).removeClass('fa-angle-up').addClass('fa-angle-down');
+});
+
+$('.panel').on('show.bs.collapse', function (el) {
+  $('#toggleangle' + el.target.id.replace('collapse', '')).removeClass('fa-angle-down').addClass('fa-angle-up');
+});
+
+$(document).ready(function() {
+  redraw(0);
+  new Clipboard('.clipboard-button');
+});
+
+gitbook.events.bind('page.change', function() {
+  redraw(0);
+});
