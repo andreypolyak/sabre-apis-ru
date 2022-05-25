3335b9b3dd7bf7655a6692b890bda38b21068d19
Andrey Polyak <andrey.polyak@gmail.com>
2022-05-25 19:04:54 +0300

    Fix scripts and CSS

--- docs/assets/js/clipboard.js
+++ docs/assets/js/clipboard.js
@@ -1,10 +1,7 @@
 const addCopyButtons = (clipboard) => {
-  // 1. Look for pre > code elements in the DOM
   document.querySelectorAll("pre > code").forEach((codeBlock) => {
-    // 2. Create a button that will trigger a copy operation
-    const button = document.createElement("button");
-    button.className = "clipboard-button";
-    button.type = "button";
+    const button = document.createElement("a");
+    button.className = "book-btn clipboard-btn";
     button.innerHTML = "Скопировать";
     button.addEventListener("click", () => {
       clipboard.writeText(codeBlock.textContent).then(
@@ -13,18 +10,11 @@ const addCopyButtons = (clipboard) => {
           button.innerHTML = "Скопировано!";
           setTimeout(() => (button.innerHTML = "Скопировать"), 2000);
         },
-        (error) => (button.innerHTML = "Error")
+        (error) => (button.innerHTML = "Ошибка")
       );
     });
-    // 3. Append the button directly before the pre tag
-    let pre = codeBlock.parentNode.parentNode.parentNode;
-    if (pre.tagName === "DETAILS") {
-      pre.parentNode.insertBefore(button, pre);
-    }
-    pre = codeBlock.parentNode.parentNode.parentNode.parentNode;
-    if (pre.tagName === "DETAILS") {
-      pre.parentNode.insertBefore(button, pre);
-    }
+    const details = parentByTag(codeBlock, "DETAILS");
+    details.parentNode.insertBefore(button, details);
   });
 };
 
@@ -32,10 +22,17 @@ if (navigator && navigator.clipboard) {
   addCopyButtons(navigator.clipboard);
 } else {
   const script = document.createElement("script");
-  script.src =
-    "https://cdnjs.cloudflare.com/ajax/libs/clipboard-polyfill/2.7.0/clipboard-polyfill.promise.js";
+  script.src = "https://cdnjs.cloudflare.com/ajax/libs/clipboard-polyfill/2.7.0/clipboard-polyfill.promise.js";
   script.integrity = "sha256-waClS2re9NUbXRsryKoof+F9qc1gjjIhc2eT7ZbIv94=";
   script.crossOrigin = "anonymous";
   script.onload = () => addCopyButtons(clipboard);
   document.body.appendChild(script);
+}
+
+function parentByTag(el, tag) {
+  if (!el || el.tagName == tag) {
+    return el
+  } else {
+    return parentByTag(el.parentElement, tag)
+  }
 }
